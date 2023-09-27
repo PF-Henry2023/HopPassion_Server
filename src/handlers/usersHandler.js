b963d70bf393298239a1d23c167b799c4e55bd4b
Henry Corredor González <henry.corredor7570@gmail.com>
2023-09-27 16:55:07 -0500

    modelo user y ruta create: HC

---
+++ src/handlers/usersHandler.js
@@ -0,0 +1,27 @@
+const { createUser, updateUser } = require("../controllers/usersController");
+
+const createUserHandler = async (req, res) => {
+    const { name, lastName, address, email, phone, role, password } = req.body;
+    try {
+        const response = await createUser(name, lastName, address, email, phone, role, password);
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+const  updateUserHandler = async (req, res) => {
+    try {
+        const { id, name, lastName, address, email, phone, role, password } = req.body;
+        if(!id) throw Error("El id es obligatorio");
+        const response = await updateUser(id, name, lastName, address, email, phone, role, password);
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+module.exports = {
+    createUserHandler,
+    updateUserHandler,
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const { Router } = require("express");
 const router = Router();
 
 const reviewRouter = require("./reviewRouter");
+const usersRouter = require("./usersRouter");
 
 router.use("/review", reviewRouter);
+router.use("/users", usersRouter);
 
 module.exports = router;
