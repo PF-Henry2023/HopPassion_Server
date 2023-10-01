50361c6ea1d938280f9e78793645dac9bf03151f
Cintia Coccia <cintia_08@hotmail.com>
2023-09-30 21:32:38 -0300

    remove legacy code

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,12 @@ const saveAllRoute = require("./saveAllInfoAPI");
 const reviewRouter = require("./reviewRouter");
 const productRouter = require("./productRouter");
 const usersRouter = require("./usersRouter");
-const filterRoute = require("./filtersRoute.js");
 const categoriesSave = require("./saveCategoriesRoute");
 const categoriesRoutes = require("./categoriesRoute");
 
 router.use("/dataUpload", saveAllRoute);
 router.use("/review", reviewRouter); 
 router.use("/users", usersRouter);
-router.use("/filters", filterRoute);
 router.use("/saveCategories", categoriesSave);
 router.use("/product", productRouter);
 router.use("/categories", categoriesRoutes);
