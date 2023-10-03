3281bfb3fd20e2bbb237dcab534a13dfb33dc646
Quena <quenallatafranco@gmail.com>
2023-10-03 19:43:05 -0300

    cambios en modelos

--- src/models/Categorie.js
+++ src/models/Categorie.js
@@ -2,24 +2,26 @@ const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 
 module.exports = (sequelize) => {
-  sequelize.define("Categorie", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+  sequelize.define(
+    "Categorie",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
 
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-    description: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  },
-
-    {timestamps: false},
+    { timestamps: false }
   );
 };
