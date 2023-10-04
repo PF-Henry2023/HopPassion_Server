5779def1eec89f163ccb087a10db5340021da618
Quena <quenallatafranco@gmail.com>
2023-10-03 22:44:44 -0300

    review

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,11 +1,77 @@
-const createRev = async () => {
+const { Review, Product } = require("../db");
+
+const createRev = async (review) => {
   try {
-    return "createRev";
+    const { idProd, ...reviewData } = review;
+    const newReview = await Review.create(reviewData);
+
+    if (idProd) {
+      const product = await Product.findByPk(idProd);
+      if (product) {
+        await newReview.setProduct(product);
+      } else {
+        throw new Error(`Producto con id ${idProd} no encontrado.`);
+      }
+    }
+
+    return newReview;
   } catch (error) {
-    throw new Error(`Error creating Review :${error.message}`);
+    throw new Error(`Error creating Review: ${error.message}`);
+  }
+};
+
+const deleteRev = async (idReview) => {
+  try {
+    const deletedReview = await Review.destroy({
+      where: {
+        id: idReview,
+      },
+    });
+
+    if (deletedReview === 0) {
+      throw new Error(`No se encontró ninguna revisión con ID ${idReview}.`);
+    }
+
+    return deletedReview;
+  } catch (error) {
+    throw new Error(`Error deleting Review: ${error.message}`);
+  }
+};
+
+const updateRev = async (idReview, changes) => {
+  try {
+    const [updatedCount, updatedReviews] = await Review.update(changes, {
+      where: {
+        id: idReview,
+      },
+      returning: true, // Esto devuelve las filas actualizadas
+    });
+
+    //updatedCount son las cant de cambios;
+
+    if (updatedCount === 0) {
+      throw new Error(`No se encontró ninguna revisión con ID ${idReview}.`);
+    }
+
+    return updatedReviews[0]; // Devuelve la revisión actualizada
+  } catch (error) {
+    throw new Error(`Error updating Review: ${error.message}`);
+  }
+};
+
+const listRev = async () => {
+  try {
+    // Utiliza el método findAll para obtener todas las revisiones
+    const reviews = await Review.findAll();
+    return reviews;
+  } catch (error) {
+    throw new Error(`Error retrieving Review: ${error.message}`);
   }
 };
 
 module.exports = {
   createRev,
+  deleteRev,
+  updateRev,
+  listRev,
 };
--- src/handlers/reviewHandler.js
+++ src/handlers/reviewHandler.js
@@ -1,11 +1,44 @@
-const { createRev } = require("../controllers/reviewController.js");
+const {
+  createRev,
+  deleteRev,
+  updateRev,
+  listRev,
+} = require("../controllers/reviewController.js");
 
 const createReview = async (req, res) => {
   try {
-    const response = await createRev();
+    const review = req.body;
+    const response = await createRev(review);
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
-module.exports = { createReview };
+const deleteReview = async (req, res) => {
+  try {
+    const { idReview } = req.body;
+    const response = await deleteRev(idReview);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const updateReview = async (req, res) => {
+  try {
+    const { idProd, ...changes } = req.body;
+    const response = await updateRev(idProd, changes);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const listReview = async (req, res) => {
+  try {
+    const response = await listRev();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { createReview, deleteReview, updateReview, listReview };
