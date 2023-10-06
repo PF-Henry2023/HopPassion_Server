a890332ffcf3bd569337ad33de5e91cd7d566c61
Henry Corredor González <henry.corredor7570@gmail.com>
2023-10-05 20:24:14 -0500

    Auth0: HC

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,7 +3,7 @@ const usersRouter = Router();
 const { checkRolesExisted, checkDuplicateUserNameOrEmail} = require ("../utils/authJwt");
 const { createUserHandler, updateUserHandler, signinHandler, getAllUsersHandler, getUserByIdHandler } = require("../handlers/usersHandler");
 
-usersRouter.post("/signup", checkDuplicateUserNameOrEmail, checkRolesExisted, createUserHandler);
+usersRouter.post("/signup", checkDuplicateUserNameOrEmail, createUserHandler); // funcion para verificar los roles: checkRolesExisted  (NO USAR POR EL MOMENTO) 
 usersRouter.post("/signin", signinHandler);
 usersRouter.put("/update/:id", updateUserHandler);
 usersRouter.get("/allUsers", getAllUsersHandler);
