2b36bb4e625db6ae6ebbf624146fe1a7a9c5eff5
spadonienzo <spadonienzo@gmail.com>
2023-10-08 22:23:59 -0300

    nuevo id

--- src/app.js
+++ src/app.js
@@ -3,37 +3,45 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const { auth } = require('express-oauth2-jwt-bearer');
+// const { auth } = require('express-oauth2-jwt-bearer');
 
 require("./db.js");
 
 const server = express();
 server.name = "API";
 
-const jwtCheck = auth({
-  audience: 'http://localhost:3001',
-  issuerBaseURL: 'https://dev-ke2kmb45sqnv6r5n.us.auth0.com/',
-  tokenSigningAlg: 'RS256'
-});
+// const jwtCheck = auth({
+//   audience: 'http://localhost:3001',
+//   issuerBaseURL: 'https://dev-ke2kmb45sqnv6r5n.us.auth0.com/',
+//   tokenSigningAlg: 'RS256'
+// });
 
-server.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
+server.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 1000000,
+  })
+);
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
 server.use("/", routes);
-server.use(jwtCheck);
+// server.use(jwtCheck);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
-
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
 const usersRouter = Router();
-const { checkRolesExisted, checkDuplicateUserNameOrEmail} = require ("../utils/authJwt");
-const { createUserHandler, updateUserHandler, signinHandler, getAllUsersHandler, getUserByIdHandler, loginOauth, signupOauth} = require("../handlers/usersHandler");
+const {
+  checkRolesExisted,
+  checkDuplicateUserNameOrEmail,
+} = require("../utils/authJwt");
+const {
+  createUserHandler,
+  updateUserHandler,
+  signinHandler,
+  getAllUsersHandler,
+  getUserByIdHandler,
+  loginOauth,
+  signupOauth,
+} = require("../handlers/usersHandler");
 
-usersRouter.post("/signup", checkDuplicateUserNameOrEmail, createUserHandler); // funcion para verificar los roles: checkRolesExisted  (NO USAR POR EL MOMENTO) 
+usersRouter.post("/signup", checkDuplicateUserNameOrEmail, createUserHandler); // funcion para verificar los roles: checkRolesExisted  (NO USAR POR EL MOMENTO)
 usersRouter.post("/signin", signinHandler);
 usersRouter.put("/update/:id", updateUserHandler);
 usersRouter.get("/allUsers", getAllUsersHandler);
