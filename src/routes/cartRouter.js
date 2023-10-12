9d498fc8748c7523681bad23232603a51f7e8ddf
Cintia Coccia <cintia_08@hotmail.com>
2023-10-12 20:08:58 -0300

    orders for user

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,14 +1,9 @@
 const { Router } = require("express");
-const { getCartHandler, addProductHandler, removeProductHandler, deleteProductHandler, setProductHandler, getAllOrderDetail } = require("../handlers/cartHandler");
+const { getCartHandler, setProductHandler } = require("../handlers/cartHandler");
 const { verifyToken } = require ("../utils/authJwt");
 const cartRouter = Router();
 
 cartRouter.get("/", verifyToken, getCartHandler);
-cartRouter.post("/add", verifyToken, addProductHandler);
-cartRouter.post("/remove", verifyToken, removeProductHandler);
-cartRouter.post("/delete", verifyToken, deleteProductHandler);
 cartRouter.put("/set", verifyToken, setProductHandler);
-cartRouter.get("/getAllOrderDetails", /* verifyToken, */ getAllOrderDetail);
 
 module.exports = cartRouter;
-
